9f2a7c27380b0eb95def116148e15b29966a3e66
Cathrine Kanini <catherinekanini001@gmail.com>
2020-07-20 16:26:17 +0300

    akan names

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 * Technologies
 * Inspiration
 * Setup
+* Behaviour driven.
 * Contributions.
 * Licence
 ### General information
@@ -21,14 +22,48 @@ Before you continue with this project, make sure you have...
 
 ### Technologies
 This project is creadted with the use of 
-* Visual studio version 1.47.
+* HTML
+* CSS
+* BOOTSTRAP
+* Javascript.
 ### Set up
-To run this project,install it locally. 
+To run this project,install it locally.
+* Clone this repository to a location in your file system 
+[git clone](https://github.com/KadogoKenya/generateNames)
+* Open terminal command line then navigate to the root folder of the application. cd AkanNameGenerator
+* Open index.html on your Browser. 
+### Behaviour Driven Development
+    1. Displays Form For Entering Date and Gender
+        ◦ Input: "User's firstname"
+        ◦ Input: "User's secondname"
+        ◦ Input: "User's birthday day"
+        ◦ Input: "User's Birth month"
+        ◦ Input: "User's Birth year"
+        ◦ Input: "User's Birth year"
+        ◦ Output: "Displays User's Akan Name According to Submitted Date and Gender"
+        
+    2. Displays an Error Message if Date Input Left Blank
+        ◦ Input: ""
+        ◦ OUTPUT: "Displays Error Message"
+    3. Displays an Error Message if Gender Not Chosen
+        ◦ INPUT: ""
+        ◦ OUTPUT: "Display an Error Message"
+    4.  Display an error massage if firstname and secondname fields are left bank.
+
 ### Inspiration
 This project is based on HTML,CSS, JS tutorials from W3Schools.
 ### Contributions
 To contribute to Akan names project, clone this repo locally and commit your code on a separate branch. write a unit code and run before opening puul requests. 
 ### Licence.
+* Author: Cathrine Kanini.
+* Application Name: Akan Names Application
+* Application Description:This is an application on akan names where one is given a name based on the day of birth which is calculated using a predefined algorithm.
+* Development date: 01/12/2019
+* Contacts: You can get in touch with me through catherinekanini001@gmail.com
+* Copyright: No one is allowed to copy this project 
+
+[Link:](https://kadogokenya.github.io/generateNames/)
+
 
 
 
--- scripts.js
+++ scripts.js
@@ -1,21 +1,126 @@
-var weekDay=["Sunday","Monday","Tuesday","Thursday","Friday","Saturday"];
-var maleName=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-var femaleName=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-
-function myNames(){
-    var day=parseInt(document.getElementById("dd").value);
-    var month=parseInt(document.getElementById("mm").value);
-    var year=parseInt(document.getElementById("yy").value);
-    var year=parseInt(year.slice(0 , 2));
-    var year=parseInt(year.slice(2 , 4));
-
-    if(dd<1 || dd>7)
-    alert("invalid days value");
-
-    if(mm<1 || mm>12);
-    alert("re-enter the value of month");
+ function calculate(day, month, yy, cc) {
+     var ss1 = cc / 4 - (2 * cc - 1);
+     var ss2 = (5 * yy) / 4;
+     var ss3 = (26 * (month + 1)) / 10;
+     var ss = ss1 + ss2 + ss3 + day;
+     ss = ss % 7;
+  
+     ss = parseInt(ss);
+     return ss;
+   }
+  
+  function myFormData() {
+    var formData = document.forms[0];
+  
+    var fname = formData.firstname.value;
+    var sname = formData.secondname.value;
+    var fullname = fname.concat(sname)
+    var day = parseInt(formData.day.value);
+    var month = parseInt(formData.month.value);
+    var year = parseInt(document.getElementById("year").value);
+   var gender = parseInt(formData.gender.value);
     
-    if(yy<1 || yy>31)
-    alert("invalid months");
-
-}
+    var cc = parseInt(year / 100);
+    var yy = year % 100;
+  
+    var ss = calculate(day, month, yy, cc);
+  
+     if (ss === 0 && gender == 1) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Sunday and your akan name is Kwasi"
+       );
+     } else if (ss === 0 && gender == 2) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Sunday and your akan name is Akosua"
+       );
+     } else if (ss === 1 && gender == 1) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Monday and your akan name is Kwadwo"
+       );
+     } else if (ss === 1 && gender == 2) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Monday and your akan name is Adwoa"
+       );
+     } else if (ss === 2 && gender == 1) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Teusday and your akan name is Kwabena"
+       );
+     } else if (ss === 2 && gender == 2) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Teusday and your akan name is Abenaa"
+       );
+     } else if (ss === 3 && gender == 1) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a wednesday and your akan name is Kwaku"
+       );
+     } else if (ss === 3 && gender == 2) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a wednesday and your akan name is Akua"
+       );
+     } else if (ss === 4 && gender == 1) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Thursday and your akan name is Yaw"
+       );
+     } else if (ss === 4 && gender == 2) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Thursday and your akan name is Yaa"
+       );
+     } else if (ss === 5 && gender == 1) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Friday and your akan name is Kofi"
+       );
+     } else if (ss === 5 && gender == 2) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Friday and your akan name is Afua"
+       );
+     } else if (ss === 6 && gender == 1) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Saturday and your akan name is Kwame"
+       );
+     } else if (ss === 6 && gender == 2) {
+       alert(
+         "Hello " +
+           fullname +
+           " you were born on a Saturday and your akan name is Ama"       );
+     } else {
+       alert("Enter a valid date");
+     }
+    }
+   function validate(){
+   
+     if (fname == null || sname == null || day == null ||month == null || year == null || gender == null) {
+       alert("Please fill all the requred fields");
+       return false;
+     }
+  
+     if (!(1 <= day <= 31)) {
+       alert("Enter a valid date!!!");
+       return false;
+     }   
+   }
